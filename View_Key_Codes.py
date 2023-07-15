2916e2404426f3f2cc080f5afce753664a8e3d22
hoerstl <hoerstl@berea.edu>
2023-07-15 11:59:40 -0400

    Small file that prints the event.Key attribute when you press a button.

---
+++ View_Key_Codes.py
@@ -0,0 +1,28 @@
+import pythoncom
+import pyWinhook
+
+def on_key_press(event):
+    print(event.Key)
+    return False
+
+
+def on_key_release(event):
+    print("released")
+    return False
+
+
+def start_hook():
+    # Create an instance of the hook manager
+    hook_manager = pyWinhook.HookManager()
+
+    # Register the callback function for keyboard events
+    hook_manager.KeyDown = on_key_press
+    hook_manager.KeyUp = on_key_release
+
+    # Set the hook and start the event loop
+    hook_manager.HookKeyboard()
+    pythoncom.PumpMessages()
+
+
+if __name__ == '__main__':
+    start_hook()
