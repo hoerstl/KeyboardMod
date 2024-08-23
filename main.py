308a30b533072ea77892e5b17d52ba31d5f30ca4
hoerstl <hoerstl@berea.edu>
2024-08-23 14:40:28 -0400

    Added another keyboard mode accessable by releasing both ctrl buttons at the same time

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ def is_shift_key(key_name):
 
 
 def process_mode_shift(event):
+    """
+    Decides whether or not to switch the keyboard into ShiftLock
+    Runs every key release.
+    """
     global keyboard_mode, last_key_released, shift_release_time
     if is_shift_key(event.Key):
         is_different = event.Key != last_key_released
@@ -29,15 +33,18 @@ def process_mode_shift(event):
             last_key_released = ''
             return True
         shift_release_time = time.time()
-    last_key_released = event.Key
 
 
 def process_mode_cap(event):
+    """
+    Decides whether or not to switch the keyboard into CapMode
+    Runs every key release.
+    """
     global keyboard_mode, cap_press_time, cap_mode_used
     if event.Key == 'Capital':
         if event.MessageName == 'key down' and keyboard_mode != 'CapMode':
             cap_mode_used.value = False
-            if keyboard_mode == 'ShiftLock':  # We should count disabling the shiftlock as a valid use case of cap mode.
+            if keyboard_mode != 'Default':  # We should count disabling another keyboard mode as a valid use case of cap mode.
                 cap_mode_used.value = True
             keyboard_mode = 'CapMode'
             # TODO: Fix a bug where someone holds down shift (or another non-letter key) and the release is blocked by capmode
@@ -49,9 +56,41 @@ def process_mode_cap(event):
             print('Leaving Cap Mode')
             if time.time() - cap_press_time < .3 and not cap_mode_used.value:
                 pressKey("Capital")
-        return False
+        return True
 
-    return True
+    return False
+
+
+
+def is_ctrl_key(key_name):
+    ctrl_names = [
+        "control",
+        "Lcontrol",
+        "Rcontrol",
+    ]
+    return key_name in ctrl_names
+
+def process_mode_ctrl(event):
+    """
+    Decides whether or not to switch the keyboard into CtrlMode
+    Runs every key release.
+    """
+    global keyboard_mode, last_key_released, ctrl_release_time
+    if is_ctrl_key(event.Key):
+        is_different = event.Key != last_key_released
+        was_quick_enough = time.time() - ctrl_release_time < .05
+        if is_ctrl_key(last_key_released) and is_different and was_quick_enough:
+            'passed all criteria'
+            if keyboard_mode == 'CtrlMode':
+                print("CtrlMode Off")
+                keyboard_mode = 'Default'
+            else:
+                print("CtrlMode On")
+                keyboard_mode = 'CtrlMode'
+
+            last_key_released = ''
+            return True
+        ctrl_release_time = time.time()
 
 
 def update():
@@ -70,11 +109,18 @@ def update():
 
 
 def on_key_press(event):
+    """
+    This function is a callback function which is called every time a key is pressed on the system.
+    
+    Return:
+    bool | If the response is True, then the keystroke is passed onto windows. If false, then the 
+    default keystroke behavior is blocked
+    """
     global keyboard_mode
     update()
     if is_press_bypassed(event):
         return True
-    if not process_mode_cap(event):
+    if process_mode_cap(event):
         return False
 
     # Process keyboard input as you wish
@@ -84,19 +130,34 @@ def on_key_press(event):
         onPress_ShiftLock(event)
     elif keyboard_mode == 'CapMode':
         onPress_CapMode(event)
+    elif keyboard_mode == 'CtrlMode':
+        onPress_CtrlMode(event)
 
     return False
 
 
 def on_key_release(event):
-    global keyboard_mode
+    """
+    This function contains logic for swapping between modes as well as for describing the behavior 
+    of blocking key releases.
+
+    Return:
+    bool | If the response is True, then the keystroke is passed onto windows. If false, then the 
+    default key release behavior is blocked
+    """
+    global keyboard_mode, last_key_released
     if is_release_bypassed(event):
         return True
 
+    # Read these if statements as "if the key falls under entering _____ mode's juristiction. Ex. shift keys fall under shiftmode etc."
     if process_mode_shift(event):
-        return True  # We need to allow the shift keys to be released when we swap to and from modes
-    if not process_mode_cap(event):
-        return False
+        return True  # We need to allow the shift keys to be released when we swap to and from modes since it involves two keys
+    if process_mode_cap(event):
+        return False # We can just block both the press and release of a single key since interacting with it at all means that we are certainly changing keyboard modes
+    if process_mode_ctrl(event):
+        return True # We need to allow the ctrl keys to be released when we swap to and from modes since it involves two keys
+    last_key_released = event.Key
+
     # Default keyboard input
     if keyboard_mode == 'Default':
         return True
@@ -105,7 +166,11 @@ def on_key_release(event):
     elif keyboard_mode == 'CapMode':
         onRelease_CapMode(event)
         return False
+    elif keyboard_mode == 'CtrlMode':
+        return False
     return True
+
+
 
 
 def start_hook():
@@ -122,10 +187,11 @@ def start_hook():
 
 
 if __name__ == '__main__':
-    keyboard_mode = 'Default'  # This can have the value 'Default', 'ShiftLock', or 'CapMode'. Starts as 'Default'
+    keyboard_mode = 'Default'  # This can have the value 'Default', 'ShiftLock', 'CapMode', or 'CtrlMode'. Starts as 'Default'
     globals.init() # Perform first time initialization of global data and environment variables
     cap_press_time = 0
     shift_release_time = 0
+    ctrl_release_time = 0
     last_key_released = ''
     start_hook()
 
--- secondaryActions.py
+++ secondaryActions.py
@@ -13,75 +13,82 @@ active_mimics = []
 held_keys = []
 cap_mode_used = Node(False)
 
-class POINT(ctypes.Structure):
-    _fields_ = [("x", ctypes.c_long), ("y", ctypes.c_long)]
 
 
 
-class Point:
 
-    def __init__(self, x=0, y=0):
-        self.x = round(x)
-        self.y = round(y)
-
-    def set(self, x, y):
-        self.x = round(x)
-        self.y = round(y)
+########################## Currently inactive code for moving the mouse with the keyboard ##########################################
+# class POINT(ctypes.Structure):
+#     _fields_ = [("x", ctypes.c_long), ("y", ctypes.c_long)]
 
 
 
-screen_width = ctypes.windll.user32.GetSystemMetrics(0)
-screen_height = ctypes.windll.user32.GetSystemMetrics(1)
+# class Point:
 
-major_tile_pos = Point()
+#     def __init__(self, x=0, y=0):
+#         self.x = round(x)
+#         self.y = round(y)
 
-
-def performSecondaryAction_mousetype(event):
-    global screen_width, screen_height, major_tile_pos
-
-    left_hand = [*"QWERTASDFGZXCVB"]
-    major_cols = 5
-    major_rows = 3
-    if len(left_hand) / major_cols != major_rows:
-        raise Exception('The left hand grid is not defined as a proper rectangle.')
-    major_column_width = screen_width // major_cols
-    major_row_height = screen_height // major_rows
-    if event.Key in left_hand:
-        major_grid_index = left_hand.index(event.Key)
-        row_index = major_grid_index // major_cols
-        column_index = major_grid_index % major_cols
-
-        major_tile_pos.set(column_index * major_column_width, row_index * major_row_height)
-
-        ctypes.windll.user32.SetCursorPos(major_tile_pos.x, major_tile_pos.y)
-
-    right_hand = [*'YUIOPHJKL', 'Oem_1', *'NM', 'Oem_Comma', 'Oem_Period', 'Oem_2']
-    minor_cols = 5
-    minor_rows = 3
-    if len(right_hand) / minor_cols != minor_rows:
-        raise Exception('The right hand grid is not defined as a proper rectangle.')
-    minor_column_width = major_column_width // minor_cols
-    minor_row_height = major_row_height // minor_rows
-    if event.Key in right_hand:
-        minor_grid_index = right_hand.index(event.Key)
-        row_index = minor_grid_index // minor_cols
-        column_index = minor_grid_index % minor_cols  # May need to stretch out these columns to fit all the way to the right
-
-        ctypes.windll.user32.SetCursorPos(major_tile_pos.x + (column_index * minor_column_width), major_tile_pos.y + (row_index * minor_row_height))
+#     def set(self, x, y):
+#         self.x = round(x)
+#         self.y = round(y)
 
 
-    if event.Key == "Space":
-        MOUSEEVENTF_LEFTDOWN = 0x0002
-        MOUSEEVENTF_LEFTUP = 0x0004
 
-        # Call the mouse_event function from the Windows API
-        ctypes.windll.user32.mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
-        ctypes.windll.user32.mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+# screen_width = ctypes.windll.user32.GetSystemMetrics(0)
+# screen_height = ctypes.windll.user32.GetSystemMetrics(1)
 
-    if event.Key in ["Rcontrol", "Lcontrol"]:
-        exit(0)
+# major_tile_pos = Point()
 
 
+# def performSecondaryAction_mousetype(event):
+#     global screen_width, screen_height, major_tile_pos
+
+#     left_hand = [*"QWERTASDFGZXCVB"]
+#     major_cols = 5
+#     major_rows = 3
+#     if len(left_hand) / major_cols != major_rows:
+#         raise Exception('The left hand grid is not defined as a proper rectangle.')
+#     major_column_width = screen_width // major_cols
+#     major_row_height = screen_height // major_rows
+#     if event.Key in left_hand:
+#         major_grid_index = left_hand.index(event.Key)
+#         row_index = major_grid_index // major_cols
+#         column_index = major_grid_index % major_cols
+
+#         major_tile_pos.set(column_index * major_column_width, row_index * major_row_height)
+
+#         ctypes.windll.user32.SetCursorPos(major_tile_pos.x, major_tile_pos.y)
+
+#     right_hand = [*'YUIOPHJKL', 'Oem_1', *'NM', 'Oem_Comma', 'Oem_Period', 'Oem_2']
+#     minor_cols = 5
+#     minor_rows = 3
+#     if len(right_hand) / minor_cols != minor_rows:
+#         raise Exception('The right hand grid is not defined as a proper rectangle.')
+#     minor_column_width = major_column_width // minor_cols
+#     minor_row_height = major_row_height // minor_rows
+#     if event.Key in right_hand:
+#         minor_grid_index = right_hand.index(event.Key)
+#         row_index = minor_grid_index // minor_cols
+#         column_index = minor_grid_index % minor_cols  # May need to stretch out these columns to fit all the way to the right
+
+#         ctypes.windll.user32.SetCursorPos(major_tile_pos.x + (column_index * minor_column_width), major_tile_pos.y + (row_index * minor_row_height))
+
+
+#     if event.Key == "Space":
+#         MOUSEEVENTF_LEFTDOWN = 0x0002
+#         MOUSEEVENTF_LEFTUP = 0x0004
+
+#         # Call the mouse_event function from the Windows API
+#         ctypes.windll.user32.mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+#         ctypes.windll.user32.mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+
+#     if event.Key in ["Rcontrol", "Lcontrol"]:
+#         exit(0)
+############################################ End Mouse Control Code #########################################################
+
+
+########################### Convenience functions for common keyboard manipulation ##########################################
 def is_press_bypassed(event):
     global keypress_bypass
     if event.Key == keypress_bypass.peek():
@@ -294,3 +301,15 @@ def onRelease_CapMode(event):
             releaseKey(keyToMimic)
             active_mimics.remove(event.Key)
         return
+
+
+
+###################### START OF THE CTRLMODE DEFINITIONS ############################
+
+
+def onPress_CtrlMode(event):
+    print("in ctrl mode")
+
+
+
+
